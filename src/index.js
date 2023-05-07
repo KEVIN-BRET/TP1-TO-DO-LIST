935e486313e58197c7f48fb1f293b167289e1010
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-05-07 22:12:06 +0200

    Fonction éditer (toggle)

--- src/index.js
+++ src/index.js
@@ -19,17 +19,23 @@ const todos = [
   {
     text: "Je suis une Todo",
     done: false,
+    editMode: true,
   },
   {
     text: "faire du JavaScript",
     done: true,
+    editMode: false,
   },
 ];
 
 // Affichage du tableau :
 const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
-    return createTodoElement(todo, index);
+    if (todo.editMode) {
+      return createTodoEditElement(todo, index);
+    } else {
+      return createTodoElement(todo, index);
+    }
   });
   ul.innerHTML = "";
   ul.append(...todosNode);
@@ -40,10 +46,17 @@ const createTodoElement = (todo, index) => {
   const li = document.createElement("li");
   const buttonDelete = document.createElement("button");
   buttonDelete.innerHTML = "Supprimer";
+  const buttonEdit = document.createElement("button");
+  buttonEdit.innerHTML = "Editer";
+
   buttonDelete.addEventListener("click", (event) => {
-	// pour eviter le toggle du li suivant
-	event.stopImmediatePropagation()
+    // pour eviter le toggle du li suivant
+    event.stopPropagation();
     deleteTodo(index);
+  });
+  buttonEdit.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleEditMode(index);
   });
   li.innerHTML = `
 		<span class="todo ${todo.done ? "done" : ""}"></span>
@@ -52,7 +65,21 @@ const createTodoElement = (todo, index) => {
   li.addEventListener("click", (event) => {
     toggleTodo(index);
   });
-  li.appendChild(buttonDelete);
+  li.append(buttonEdit, buttonDelete);
+  return li;
+};
+
+// Format d'édition d'un "todo" :
+const createTodoEditElement = (todo, index) => {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = todo.text;
+  const buttonSave = document.createElement("button");
+  buttonSave.innerHTML = "Save";
+  const buttonCancel = document.createElement("button");
+  buttonCancel.innerHTML = "Cancel";
+  li.append(input, buttonCancel, buttonSave);
   return li;
 };
 
@@ -74,6 +101,12 @@ const deleteTodo = (index) => {
 // Cocher / décocher un "todo" :
 const toggleTodo = (index) => {
   todos[index].done = !todos[index].done;
+  displayTodo();
+};
+
+// Passer en mode édition (ou non) :
+const toggleEditMode = (index) => {
+  todos[index].editMode = !todos[index].editMode;
   displayTodo();
 };
 
